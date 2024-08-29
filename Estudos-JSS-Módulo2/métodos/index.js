b2aae493c0e6c2fe87a98af20433db324c948fd4
Jhonatan Fagundes <149586964+Jhonatanfagundess@users.noreply.github.com>
2024-08-29 00:37:29 -0300

    Inserindo arquivos de outra pasta
    Aqui estou inserindo arquivos do resto da matéria desse módulo

---
+++ Estudos-JSS-Módulo2/métodos/index.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    name : 'Jhonatan',
+    idade : 22,
+    dizerOla(){
+        console.log('Hello, my name is ' + this.name)
+    }
+}
+
+console.log(pessoa)
+
+pessoa.dizerOla()
